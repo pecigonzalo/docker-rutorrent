cd14331666920bebb0aead80239c46027229e762
sparklyballs <sparklyballs@gmail.com>
2015-08-14 02:20:31 +0100

    save up for the night

--- init/30_set_folders_and_config.sh
+++ init/30_set_folders_and_config.sh
@@ -13,9 +13,5 @@ if [ ! -d "/config/rtorrent/rtorrent_sess" ]; then
 mkdir -p /config/rtorrent/rtorrent_sess
 fi
 
-if [ -f "/config/nginx/site-confs/default" ]; then
-rm /config/nginx/site-confs/default
-cp /defaults/rutorrent.www /config/nginx/site-confs/rutorrent
-fi
 chown abc:abc -R /config
 chown abc:abc /downloads
