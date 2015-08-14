b867facd5de3377e169162055096d451d8bbea89
sparklyballs <sparklyballs@gmail.com>
2015-08-14 09:13:45 +0100

    clear hanging .dtach file from previous session

--- init/90_start_rtorrent.sh
+++ init/90_start_rtorrent.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
+
+# clear hanging .dtach file from previous session
+if [ -f "/config/rtorrent/rtorrent_sess/.dtach" ]; then
+rm 
+/config/rtorrent/rtorrent_sess/.dtach
+sleep 1s
+fi
+
 dtach -n /config/rtorrent/rtorrent_sess/.dtach /sbin/setuser abc /usr/bin/rtorrent -n -o import=/config/rtorrent/rtorrent.rc
